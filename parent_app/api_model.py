cbe42ca3e4cb73f5faad7aee5c369e6306a8b8eb
aashishsamarth23 <aashishsamarth11@gmail.com>
2024-08-22 22:53:44 +0530

    Add files via upload

---
+++ parent_app/api_model.py
@@ -0,0 +1,22 @@
+from flask_restx import fields
+from extensions import api
+first_model = api.model('data1', {
+    "id": fields.Integer,
+    "name": fields.String,
+    "course": fields.String
+})
+second_model = api.model('data2', {
+    "id": fields.Integer,
+    "course": fields.String,
+    "difficulty": fields.String
+})
+first_input_model = api.model("data1_input", {
+    
+    "name": fields.String,
+    "course": fields.String
+})
+second_input_model = api.model("data2_input", {
+    
+    "course": fields.String,
+    "difficulty": fields.String
+})
---
+++ parent_app/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+class data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    course = db.Column(db.String(200), nullable = False)
+class course(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    course = db.Column(db.String(100), nullable = False)
+    difficulty = db.Column(db.String(200), nullable = False)
